3c7094c0d976ba0a6a5a4c690445d5873c5a0b56
win <samihamuhabathulla@gmail.com>
2026-07-07 20:43:03 +0530

    login feature added

--- src/main/support/CustomWorld.ts
+++ src/main/support/CustomWorld.ts
@@ -1,8 +1,10 @@
 import { BrowserContext, Browser, Page } from "@playwright/test";
+import { LoginPage } from "../../test/pages/LoginPage";
 
 export class CustomWorld {
     browser!:Browser;
     context!:BrowserContext;
     page!:Page;
+    loginPage!:LoginPage;
 
 }
