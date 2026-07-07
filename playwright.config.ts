18ada682185723ab2076eb173264b07c0ec20bd1
Balamurugan <murugan020325@gmail.com>
2026-07-07 17:02:58 +0530

    Update dotenv config path for environment variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, 'env/.env') });
+dotenv.config({ path: path.resolve(__dirname, 'src/resources/env/.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
