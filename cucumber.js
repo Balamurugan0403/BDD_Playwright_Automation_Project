75a4fc454b36556b04e5a1a70ea5ca88299479ef
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-07-08 10:27:30 +0530

    Execution set to sequential and HTML report added

--- cucumber.js
+++ cucumber.js
@@ -19,12 +19,13 @@ module.exports = {
         dryRun: false,
         format: [
       "progress",
-      "rerun:rerun/@rerun.txt",
-      "json:reports/cucumber-report.json",
-      "message:reports/messages.ndjson",
-      "allure-cucumberjs/reporter"
+    "allure-cucumberjs/reporter",
+    "json:reports/cucumber-report.json",
+    "html:reports/cucumber-report.html",
+    "message:reports/messages.ndjson",
+    "rerun:rerun/@rerun.txt"
     ],
-        parallel: 2
+        parallel: 1
     },
     rerun:{
         requireModule: ["ts-node/register"],
@@ -42,10 +43,11 @@ module.exports = {
         dryRun: false,
         format: [
       "progress",
-      "rerun:rerun/@rerun.txt",
-      "json:reports/cucumber-report.json",
-      "message:reports/messages.ndjson",
-      "allure-cucumberjs/reporter"
+    "allure-cucumberjs/reporter",
+    "json:reports/cucumber-report.json",
+    "html:reports/cucumber-report.html",
+    "message:reports/messages.ndjson",
+    "rerun:rerun/@rerun.txt"
     ],
         parallel: 1
     }
